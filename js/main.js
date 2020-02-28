0c3adb500548710141ebaaabf65470fb6026c464
murad-narimanli <51201785+murad-narimanli@users.noreply.github.com>
2020-02-28 12:48:58 +0400

    sssss

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@
 
 $(".toggle-btn").click(function () {
 	$("#sidebar").toggleClass("active");
+	$("#overlay").toggleClass("blackoverlay");
+});
+
+$("#overlay").click(function () {
+	$("#sidebar").removeClass("active");
+	$("#overlay").removeClass("blackoverlay");
+});
+
+
+$(".lang").click(function () {
+	$('.opened').slideToggle();
+	$('.nextpng img').toggleClass("iconrotate");
 });
