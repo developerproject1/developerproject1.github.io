ecb6ff449cd0378bb68e3416524024e86e20a3a4
murad-narimanli <51201785+murad-narimanli@users.noreply.github.com>
2020-03-06 16:15:01 +0400

    9

--- js/main.js
+++ js/main.js
@@ -2,6 +2,35 @@
 //   document.getElementById("sidebar").classList.toggle("active");
 // }
 
+var bar = new ProgressBar.Circle(pbCircle, {
+  color: '#000',
+  strokeWidth: 1,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 2000,
+  text: {
+    autoStyleContainer: true
+  },
+  from: { color: '#ffc107', width: 1 },
+  to: { color: '#f3a35a', width: 1 }, 
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+  }
+});
+	bar.animate(1);
+ // Number from 0.0 to 1.0
+// stroke-dashoffset: 310.985;
+	let variable = 1;
+ $("#carouselExampleCaptions").on('slide.bs.carousel', function () {
+ 	$('#carouselExampleCaptions').find('.progressbar-circle').find('path').animate({'stroke-dashoffset': 311.061}, 0)
+ 		bar.animate(variable);
+	});
+
+$('.carousel').carousel({
+  interval: 2000
+})
+
 $(".toggle-btn").click(function () {
 	$("#sidebar").toggleClass("active");
 	$("#overlay").toggleClass("blackoverlay");
