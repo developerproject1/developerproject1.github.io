28022d9f384c4f6740124e997fbf52ac1553b1bf
murad-narimanli <51201785+murad-narimanli@users.noreply.github.com>
2020-05-18 11:20:18 +0400

    sss

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@
 
             $("#nav-menu").addClass("fixednav");
               // $(".info").addClass("d-none");
-              $(".euro").attr('src', '/img/goy.png');
+              $(".euro").attr('src', 'img/goy.png');
 
          }
           if ($(window).scrollTop()<30){
                   $("#nav-menu").removeClass("fixednav" );
                           // $(".info").removeClass("d-none");
-                  $(".euro").attr('src', '/img/ag.png');
+                  $(".euro").attr('src', 'img/ag.png');
          }
       
      }
